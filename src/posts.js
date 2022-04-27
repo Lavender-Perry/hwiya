b9d07dc05a9e1a22fd49e56d9bed6a53cd2fb825
Lavender Perry <endie2@protonmail.com>
2022-04-26 18:13:12 -0700

    Switch to node-fetch for my own sanity.

--- src/posts.js
+++ src/posts.js
@@ -1,22 +1,24 @@
-import * as https from "https"
+import fetch from "node-fetch";
 
-export default let posts = {
+let posts = {
     safebooru: undefined
 };
 
-export function getPosts(site) {
-    const site_info = {
-        // [hostname, path]
-        safebooru: ["safebooru.org", "/index.php?page=dapi&s=post&q=index&json=1"]
+export default posts;
+
+async function getPosts(site) {
+    const urls = {
+        safebooru: "https://safebooru.org/index.php?page=dapi&s=post&q=index&json=1"
     };
-    const req = https.request({
-        hostname: site_info[site][0],
-        port: 443,
-        path: site_info[site][1],
-        method: "GET"
-    }, res => res.on("data", data => posts[site] = data));
-    req.on("error", console.error);
+    const ret = await fetch(urls[site]);
+    if (!ret.ok) {
+        console.log("Error getting " + site + " posts: " + ret.status);
+        return;
+    }
+    posts[site] = await ret.json();
 
     setTimeout(getPosts, 3600000, site); // Repeat in 1 hour
 }
+
+getPosts("safebooru");
 
