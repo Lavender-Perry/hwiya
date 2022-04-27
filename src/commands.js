dbc6f7014272ecf4318dc7b2e6f9e25c99783ec2
Lavender Perry <endie2@protonmail.com>
2022-04-26 18:38:27 -0700

    First command done.

--- src/commands.js
+++ src/commands.js
@@ -6,9 +6,14 @@ const commands = [
         data: new SlashCommandBuilder().setName("safebooru")
             .setDescription("Finds a random image from Safebooru."),
         async execute(interaction) {
-            // TODO: actually finish this
-            console.log(posts["safebooru"]); // DEBUG
-            await interaction.reply("Not done yet, sorry.");
+            const post = posts.safebooru[
+                Math.floor(Math.random() * posts.safebooru.length)
+            ];
+            // TODO: actual embed
+            await interaction.reply(
+                `https://safebooru.org/images/${post.directory}/${post.image}
+                \n(from https://safebooru.org/index.php?page=post&s=view&id=${post.id}` 
+            );
         }
     }
 ];
