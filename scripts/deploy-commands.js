e2bb2a9ee331c897313f698d2c7f3598616621c9
Lavender Perry <endie2@protonmail.com>
2022-04-28 20:28:44 -0700

    Turns out you don't even need to unregister them... oops.

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -12,9 +12,6 @@ const route = process.argv.length >= 4 ?
     Routes.applicationGuildCommands(process.argv[2], process.argv[3]) :
     Routes.applicationCommands(process.argv[2]);
 
-rest.put(route, {body: "{}"})
-    .then(() => console.log("Successfully cleared commands."))
-    .catch(console.error);
 rest.put(route, {body: commands.map(command => command.data.toJSON())})
     .then(() => console.log("Successfully registered commands."))
     .catch(console.error);
